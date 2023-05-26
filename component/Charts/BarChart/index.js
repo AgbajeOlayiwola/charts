ef65f078448ed0686077a2f9fbf470496e76d191
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2023-05-26 21:03:50 +0100

    first commit

--- component/Charts/BarChart/index.js
+++ component/Charts/BarChart/index.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Bar } from 'react-chartjs-2'
-const BarChart = ({ chartData }) => {
+const BarChart = () => {
+  const [chartData, setChartData] = useState({
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        data: [30, 50, 20],
+        backgroundColor: ['red', 'blue', 'yellow'],
+      },
+    ],
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    const updatedData = [...chartData.datasets[0].data]
+    const dataIndex = chartData.labels.indexOf(name)
+    updatedData[dataIndex] = Number(value)
+
+    setChartData((prevChartData) => ({
+      ...prevChartData,
+      datasets: [
+        {
+          ...prevChartData.datasets[0],
+          data: updatedData,
+        },
+      ],
+    }))
+  }
+
   return (
     <div className="chart-container">
       <h2 style={{ textAlign: 'center' }}>Bar Chart</h2>
+      <input
+        type="text"
+        name="Red"
+        onChange={handleInputChange}
+        value={chartData.datasets[0].data[0]}
+      />
+      <input
+        type="text"
+        name="Blue"
+        onChange={handleInputChange}
+        value={chartData.datasets[0].data[1]}
+      />
+      <input
+        type="text"
+        name="Yellow"
+        onChange={handleInputChange}
+        value={chartData.datasets[0].data[2]}
+      />
       <Bar
         data={chartData}
         options={{
--- component/Charts/PieChart/index.js
+++ component/Charts/PieChart/index.js
@@ -1,11 +1,56 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Chart from 'chart.js'
 import 'chart.js/auto'
 import { Pie } from 'react-chartjs-2'
-const PieChart = ({ chartData }) => {
+const PieChart = () => {
+  const [chartData, setChartData] = useState({
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        data: [30, 50, 20],
+        backgroundColor: ['red', 'blue', 'yellow'],
+      },
+    ],
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    const updatedData = [...chartData.datasets[0].data]
+    const dataIndex = chartData.labels.indexOf(name)
+    updatedData[dataIndex] = Number(value)
+
+    setChartData((prevChartData) => ({
+      ...prevChartData,
+      datasets: [
+        {
+          ...prevChartData.datasets[0],
+          data: updatedData,
+        },
+      ],
+    }))
+  }
+
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>Pie Chart</h2>
+      <input
+        type="text"
+        name="Red"
+        onChange={handleInputChange}
+        value={chartData.datasets[0].data[0]}
+      />
+      <input
+        type="text"
+        name="Blue"
+        onChange={handleInputChange}
+        value={chartData.datasets[0].data[1]}
+      />
+      <input
+        type="text"
+        name="Yellow"
+        onChange={handleInputChange}
+        value={chartData.datasets[0].data[2]}
+      />
       <Pie
         data={chartData}
         options={{
